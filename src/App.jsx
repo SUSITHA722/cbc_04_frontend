8b765f077981ccc50472ebfa4153c0e8b78caf35
susitha <susithasubhashana23@gmail.com>
2025-05-01 01:02:23 +0530

    day 12

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import "./App.css"
 import AdminPage from "./pages/adminPage"
 import LoginPage from "./pages/loginPage"
+import TestingPage from "./pages/testingPage"
+import { Toaster } from "react-hot-toast"
 
 function App() {
     return (
      <BrowserRouter>
+     <Toaster position="top-right"/>
          <Routes path="/*">
               <Route path="/admin/*" element={<AdminPage/>} />
               <Route path="/login" element={<LoginPage/>} />
+              <Route path="/*" element={<TestingPage/>}/>
               <Route path="/" element={<h1>Home</h1>}/>
               <Route path="*" element={<h1>404 Not Found</h1>}/>
+              
 
          </Routes>
       </BrowserRouter>
---
+++ src/pages/testingPage.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function TestingPage() {
+   const [number,setNumber] = useState(0)
+   const [status , setStatus] = useState("Panding")  
+
+    function increment() {
+        let newValue = number + 1;
+       setNumber(newValue)
+    }
+
+    function decrement() {
+      let newValue = number - 1;
+      setNumber(newValue)
+    }
+
+    return (
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+           <span className="text-2xl font-bold">{number}</span>
+           <div className="w-full flex justify-center">
+            <button onClick={increment} className="bg-blue-500 text-white p-2 rounded-lg w-[60px] cursor-pointer">+</button>
+            <button onClick={decrement} className="bg-blue-500 text-white p-2 rounded-lg w-[60px] cursor-pointer">-</button>
+
+           </div>
+           <span className="text-2xl font-bold">{status}</span>
+           <div className="w-full flex justify-center">
+            <button onClick={
+                ()=>{
+                  setStatus("Passed")
+                }
+            } className="bg-blue-500 text-white p-2 rounded-lg w-[60px] cursor-pointer">Pass</button>
+            <button onClick={
+                ()=>{
+                   setStatus("Failed")
+                }
+            } className="bg-blue-500 text-white p-2 rounded-lg w-[60px] cursor-pointer">Fail</button>
+
+           </div>
+        </div>
+    )
+}
